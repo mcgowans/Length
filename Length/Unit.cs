8e15c62287cc4af432d1788ef0213894b783ac34
Michael McMullin <m.mcmullin@mcgowansprint.com>
2019-08-06 18:08:27 +0100

    Adds basic Unit class plus some predefined units

---
+++ Length/Unit.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="Unit.cs" company="McGowans Print">
+// Copyright (c) McGowans Print. All rights reserved.
+// </copyright>
+
+namespace Length
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Represents a unit of physical measurement.
+    /// </summary>
+    public class Unit
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Unit"/> class
+        /// </summary>
+        /// <param name="multiplier">The value to multiply a length in meters by to convert it to this <see cref="Unit"/>.</param>
+        /// <param name="name">The name of this <see cref="Unit"/>.</param>
+        /// <param name="abbreviation">The abbreviated form of this <see cref="Unit"/>.</param>
+        public Unit(double multiplier, string name, string abbreviation)
+        {
+            this.Multiplier = multiplier;
+            this.Name = name;
+            this.Abbreviation = abbreviation;
+        }
+
+        /// <summary>
+        /// Gets the value to multiply a length in meters by to convert it to this <see cref="Unit"/>.
+        /// </summary>
+        public double Multiplier { get; }
+
+        /// <summary>
+        /// Gets the name of this <see cref="Unit"/>.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the abbreviated form of this <see cref="Unit"/>.
+        /// </summary>
+        public string Abbreviation { get; }
+    }
+}
---
+++ Length/Units.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="Unit.cs" company="McGowans Print">
+// Copyright (c) McGowans Print. All rights reserved.
+// </copyright>
+
+namespace Length
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Provides predefined units for convenience.
+    /// </summary>
+    public static class Units
+    {
+        public static readonly Unit Centimeters = new Unit(0.01, "centimeters", "cm");
+        public static readonly Unit Inches = new Unit(0.0254, "inches", "in");
+        public static readonly Unit Meters = new Unit(1.0, "meters", "m");
+    }
+}
